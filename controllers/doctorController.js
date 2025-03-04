3de01aa8ef97816efbcfdb311138e351a40bd07f
RandimaD25 <randimamethminid525@gmail.com>
2025-03-04 11:45:02 +0530

    feat: implement doctor profile endpoints

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -139,6 +139,32 @@ const doctorDashboard = async (req, res) => {
   }
 };
 
+//API to get doctor profile for Doctor Panel
+const doctorProfile = async (req, res) => {
+  try {
+    const { docId } = req.body;
+    const profileData = await doctorModel.findById(docId).select("-password");
+
+    res.json({ success: true, profileData });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// API to update doctor profile data from Doctor Panel
+const updateDoctorProfile = async (req, res) => {
+  try {
+    const { docId, fees, address, available } = req.body;
+    await doctorModel.findByIdAndUpdate(docId, { fees, address, available });
+
+    res.json({ success: true, message: "Profile Updated." });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
 export {
   changeAvailability,
   doctorList,
@@ -147,4 +173,6 @@ export {
   appointmentComplete,
   appointmentCancel,
   doctorDashboard,
+  doctorProfile,
+  updateDoctorProfile,
 };
